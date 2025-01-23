b6c2ef3adebb1990554f5785c91f4563056afee2
DuralTxStudent <duralekt@gmail.com>
2025-01-23 15:06:17 +0100

    Pchamy pierwszy projekt w springu

---
+++ src/main/java/pl/edu/vistula/biedaprojektspringtomaszdural/BiedaProjektSpringTomaszDuralApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.biedaprojektspringtomaszdural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiedaProjektSpringTomaszDuralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiedaProjektSpringTomaszDuralApplication.class, args);
+    }
+
+}
